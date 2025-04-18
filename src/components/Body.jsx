2317ddfede0f4891325684b2cb1c08e5895ddff2
anandkishore06 <anandkishore060@gmail.com>
2025-04-18 17:44:36 +0530

    redirect to login if user is not authorized

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
-import NavBar from './NavBar'
-import { Outlet } from 'react-router-dom'
-import Footer from './Footer'
+import React, { useEffect } from "react";
+import NavBar from "./NavBar";
+import { Outlet, useNavigate } from "react-router-dom";
+import Footer from "./Footer";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import {addUser} from "../utils/userSlice";
 
 const Body = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const fetchUser = async () => {
+
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      console.log(res);
+      
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.status === 401) {
+        navigate("/login");
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div>
       <NavBar />
       <Outlet />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
